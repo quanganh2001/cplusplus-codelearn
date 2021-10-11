ef99c3af38d8a25061ef0f9913ccdc2f1b5a877b
quanganh2001 <89558595+quanganh2001@users.noreply.github.com>
2021-10-11 15:26:30 +0700

    Chỉnh sửa comment trong C++
    Thêm nhân chia trong C++

--- basic/ex2.cpp
+++ basic/ex2.cpp
@@ -1,4 +1,4 @@
-// Bạn hãy viết chương trình hiển thị ra màn hình tổng, hiệu trong C++
+// Bạn hãy viết chương trình hiển thị ra màn hình tính tổng, hiệu, nhân, chia trong C++
 #include <iostream>
 using namespace std;
 
@@ -6,5 +6,7 @@ int main() {
 	cout << 12 + 15 << endl;
 	cout << "1489 + 7348 = " << 1489 + 7348 << endl;
 	cout << "3472 - 2355 = " << 3472 - 2355 << endl;
+	cout << "2468 * 1234 = " << 2468 * 1234 << endl;
+	cout << "2468 / 1234 = " << 2468 / 1234 << endl;	
 	return 0;
 }
