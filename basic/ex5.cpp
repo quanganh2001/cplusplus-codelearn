20d6bfcd86b3751be380b8f8f8d05d590166689f
quanganh2001 <cocoi5659@gmail.com>
2021-10-11 15:49:23 +0700

    Hiển thị tên, năm sinh trong C++

---
+++ basic/ex5.cpp
@@ -0,0 +1,13 @@
+// Bạn hãy viết chương trình hiển thị ra màn hình thông tin sau:
+
+// Name: Codelearn.
+// Date of birth: 2019.
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Name: Codelearn.\n";
+	cout << "Date of birth: 2019.";
+	return 0;
+}
