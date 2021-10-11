d2d35b97760d250369ebf488e4451b750cf8c29b
quanganh2001 <cocoi5659@gmail.com>
2021-10-11 15:48:22 +0700

    Hiển thị kết quả mỗi phép toán trong C++

---
+++ basic/ex4.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	cout << "2468 + 1234 = " << 2468 + 1234 << endl;
+	cout << "2468 - 1234 = " << 2468 - 1234 << endl;
+	cout << "2468 * 1234 = " << 2468 * 1234 << endl;
+	cout << "2468 / 1234 = " << 2468 / 1234 << endl;	
+	return 0;
+}
